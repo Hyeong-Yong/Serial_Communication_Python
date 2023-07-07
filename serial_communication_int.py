aa7c70efad2d2207c75ecdf2087e97ec35931c89
황형용 <hwanghyeongyoung@gmail.com>
2023-07-07 18:39:28 +0900

    convert ascii to int (MCU to PC)

--- serial_communication_int.py
+++ serial_communication_int.py
@@ -7,15 +7,15 @@ import serial # conda install serial
 
 
 ############################## START 초기화 코드  ########################################
-py_serial = serial.Serial(
+ser = serial.Serial(
     # Window
-    port='COM4',
+    port='COM30',
     # 보드 레이트 (통신 속도)
     baudrate=57600,
 )
 
-if (py_serial.is_open == False):
-    py_serial.open()
+if (ser.is_open == False):
+    ser.open()
 ############################## END 초기화 코드  ####################################
 
 
@@ -28,10 +28,10 @@ byte_2= viscosity_value>>8  & 0x000000ff
 byte_3= viscosity_value>>16 & 0x000000ff
 byte_4= viscosity_value>>24 & 0x000000ff
 
-py_serial.write(bytes(byte_1.to_bytes(1, byteorder="little")))
-py_serial.write(bytes(byte_2.to_bytes(1, byteorder="little")))
-py_serial.write(bytes(byte_3.to_bytes(1, byteorder="little")))
-py_serial.write(bytes(byte_4.to_bytes(1, byteorder="little")))
+ser.write(bytes(byte_1.to_bytes(1, byteorder="little")))
+ser.write(bytes(byte_2.to_bytes(1, byteorder="little")))
+ser.write(bytes(byte_3.to_bytes(1, byteorder="little")))
+ser.write(bytes(byte_4.to_bytes(1, byteorder="little")))
 print(byte_1)
 print(byte_1.to_bytes(1, byteorder="little"))
 ############################# END 점도센서값 전송(PC to MCU) ####################
@@ -39,17 +39,21 @@ print(byte_1.to_bytes(1, byteorder="little"))
 
 
 ######################### START 온도센서값 전송(MCU to PC)######################
-
-
-
+try:
+    while True:
+            if ser.readable():
+                res= ser.read(1)
+                res_int=ord(res)
+                print(res_int)
+except KeyboardInterrupt:
+    pass
 ######################### END 온도센서값 전송(MCU to PC)########################
-while True:
     
     
 
 ############################# START 프로그램 종료 ############################
 
-py_serial.close()
+ser.close()
 
 ############################# END 프로그램 종료 ##############################
 
